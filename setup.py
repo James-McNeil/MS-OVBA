cec676861f452fb95c99102011a44dbc1462e903
C0ntr0lledCha0s <james.mcneil@bdo.co.nz>
2025-10-23 15:25:17 +1300

    Update dependencies in pyproject.toml, requirements.txt, and setup.py for improved package management

--- setup.py
+++ setup.py
@@ -4,13 +4,19 @@ setup(
     name="vbaProjectCompiler",
     packages=find_packages(),
     install_requires=[
-        # Core dependencies will be installed from requirements.txt
+        "ms_ovba_compression",
+        "ms_ovba_crypto",
+        "ms_cfb @ git+https://github.com/James-McNeil/MS-CFB@main",
+        "ms_pcode_assembler @ git+https://github.com/James-McNeil/MS-Pcode-Assembler@main",
     ],
     extras_require={
         "tests": [
             "pytest>=7.0",
             "pytest-cov>=4.0",
             "flake8>=6.0",
+            "flake8-annotations",
+            "mypy",
+            "pep8-naming",
             "coveralls>=3.0",
         ],
     },
